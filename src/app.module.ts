3bb8d082ea8c6f822786da0bcc5e1c741f0d46ed
wlgns1501 <wlgns1501@gmail.com>
2023-05-04 14:56:32 +0900

    admin setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'config/.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: true,
+      entities: ['dist/src/entities/**/*{.js,.ts}'],
+      migrations: ['dist/migration/**/*{.js,.ts}'],
+      subscribers: ['dist/subscribers/**/*{.js,.ts}'],
+      cli: {
+        entitiesDir: 'entities',
+        migrationsDir: 'migration',
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/repositories/license.repository.ts
@@ -0,0 +1,56 @@
+import { CreateLicenseDto } from 'src/license/dtos/createLicense.dto';
+import { License } from 'src/entities/lecense.entity';
+import { User } from 'src/entities/user.entity';
+import { GetLicensesDto } from 'src/license/dtos/getLicensesDto';
+import { EntityRepository } from 'typeorm';
+import { BaseRepository } from 'typeorm-transactional-cls-hooked';
+import { UpdateLicenseDto } from 'src/license/dtos/updateLicense.dto';
+
+@EntityRepository(License)
+export class LicenseRepository extends BaseRepository<License> {
+  async getLicensesList(user: User, getLicensesDto: GetLicensesDto) {
+    const { page, pageSize } = getLicensesDto;
+
+    return await this.find({
+      where: {
+        user,
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+      skip: (page - 1) * 10,
+      take: pageSize,
+    });
+  }
+
+  async createLicense(user: User, createLicenseDto: CreateLicenseDto) {
+    const { name, imageUrl } = createLicenseDto;
+
+    this.create({
+      name,
+      imageUrl,
+      user,
+    }).save();
+  }
+
+  async getLicense(licenseId: number, user: User) {
+    return this.findOne({ id: licenseId, user });
+  }
+
+  async updateLicense(
+    licenseId: number,
+    updateLicenseDto: UpdateLicenseDto,
+    user: User,
+  ) {
+    const { name, imageUrl } = updateLicenseDto;
+    return this.update({ id: licenseId, user }, { name, imageUrl });
+  }
+
+  async deleteLicense(licenseId: number, user: User) {
+    return this.delete({ id: licenseId, user });
+  }
+
+  async getActiveLicenses(user: User) {
+    return this.find({ user, status: 'ACTIVE' });
+  }
+}
